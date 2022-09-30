dbd102f31e9e9e10753962e338bf1b809ce40685
kerwindows <kerwindows@hotmail.com>
2022-09-29 20:00:06 -0400

    Updates

---
+++ precache-manifest.c0f652d07890e5daae86ccba745db2be.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18de59d7edc5e605450f34b0ce5235a",
+    "url": "/react-props-v1/index.html"
+  },
+  {
+    "revision": "97dfd96ad111ee2a9783",
+    "url": "/react-props-v1/static/js/2.eee4a93f.chunk.js"
+  },
+  {
+    "revision": "9cfc1dc339b58dbd1181",
+    "url": "/react-props-v1/static/js/main.876e04f9.chunk.js"
+  },
+  {
+    "revision": "488a52a2cbabf1001dd4",
+    "url": "/react-props-v1/static/js/runtime-main.8560506c.js"
+  }
+]);
